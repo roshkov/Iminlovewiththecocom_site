7c7f32f0fd77a58fee506319175f470cc6f33264
Nikita <266900@via.dk>
2019-04-10 12:38:25 +0200

    first phase

---
+++ js/script.js
@@ -0,0 +1,16 @@
+window.oncontextmenu = function () {
+            return false;
+        }
+        $(document).keydown(function (event) {
+            if (event.keyCode == 123) {
+                return false;
+            }
+            else if ((event.ctrlKey && event.shiftKey && event.keyCode == 73) || (event.ctrlKey && event.shiftKey && event.keyCode == 74)) {
+                return false;
+            }
+        });
+
+$("body").on("contextmenu", "img", function(e) {
+  return false;
+});
+
